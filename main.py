6306436fcc77454cb788a3abdf4e650d921d0b87
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-01-30 15:26:11 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# 2
+
+fruits = ['apple', 'banana', 'cherry']
+
+
+fruits.append('kiwi')
+
+fruits.remove('banana')
+
+cheryy = fruits.index('cherry')
+fruits.insert(cheryy, 'peach')
+
+fruits.sort()
+
+print(fruits)
